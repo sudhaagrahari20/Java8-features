b6e331c3d4e9d6855b15103f64eb42eb7d95bd6d
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-08-25 13:14:14 +0530

    Create functional_Interface.java

---
+++ functional_Interface.java
@@ -0,0 +1,14 @@
+package lambda;
+
+//@FunctionalInterface
+
+public interface MyInterface {
+    public void sayHello();
+    default  void sayBye(){
+
+    };
+    public static void sayOk(){
+
+    };
+    
+}
