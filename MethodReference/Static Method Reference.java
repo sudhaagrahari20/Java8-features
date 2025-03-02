9d5855f5785a62ab95d88607908d98afd8e6c233
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2025-03-02 16:25:25 +0530

    Create Static Method Reference.java

---
+++ MethodReference/Static Method Reference.java
@@ -0,0 +1,23 @@
+//Example-1
+public class Main{
+    public static void sayHello(){
+        System.out.println("Hello Sudha..!");
+    }
+    public static void main(String[] args){
+        Runnable ref = Main::sayHello;
+        ref.run();
+    }
+}
+
+//Example-2
+class Student{
+    static void studentName(){
+        System.out.println("My name is: Ram..!");
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Runnable ref = Student::studentName;
+        ref.run();
+    }
+}
